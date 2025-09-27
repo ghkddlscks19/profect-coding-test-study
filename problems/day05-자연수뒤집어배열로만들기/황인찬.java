3b179b851d9805f9be9f935b1bdbf5835b96c580
황인찬 <ghkddlscks19@naver.com>
2025-09-27 11:37:26 +0900

    Day 5: 황인찬

---
+++ problems/day05-자연수뒤집어배열로만들기/황인찬.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Solution {
+    public int[] solution(long n) {
+        String s = String.valueOf(n);
+        
+        int[] answer = new int[s.length()];
+        
+        for(int i =0; i<s.length(); i++) {
+            answer[i] = s.charAt(s.length() - i - 1) - '0'; // char -> int
+        }
+        
+        return answer;
+    }
+}
