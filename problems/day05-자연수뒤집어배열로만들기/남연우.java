368dce1c3d1ad218f464d9e1b0029c433514c3b4
NamYeonW00 <114655545+NamYeonW00@users.noreply.github.com>
2025-09-27 23:18:45 +0900

    Create 남연우.java

---
+++ problems/day05-자연수뒤집어배열로만들기/남연우.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(long n) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        
+        while (n > 0) {
+            list.add((int)(n % 10));
+            n /= 10;
+        }
+        
+        int answer[] = new int[list.size()];
+        
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+        
+        return answer;
+    }
+}
