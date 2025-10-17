25a68179af18249ee904a30aaa9d04a0d3751d9b
eunkyung <melon0802@naver.com>
2025-10-17 23:19:27 +0900

    Day 25: 배은경

---
+++ problems/day25-Hindex/배은경.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] citations) {
+        int answer = 0;
+        Arrays.sort(citations);
+        
+        int size = citations.length;
+        for(int i = 0; i < size; i++)
+            if(citations[i] >= size - i) {
+                answer = size - i;
+                break;
+            }
+      
+        return answer;
+    }
+}
