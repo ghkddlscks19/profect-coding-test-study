147caa63ec6bcf7a95e6a162b9444a569556acb2
eunkyung <melon0802@naver.com>
2025-10-25 23:25:57 +0900

    Day 33: 배은경

---
+++ problems/day33-전화번호목록/배은경.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        HashSet<String> set = new HashSet<>();
+        
+        for(int i = 0; i < phone_book.length; i++) 
+            set.add(phone_book[i]);
+        
+        for(int i = 0; i < phone_book.length; i++) {
+            for(int j = 1; j < phone_book[i].length(); j++) {
+                String p = phone_book[i].substring(0, j);
+                if(set.contains(p))
+                    return false;
+            }
+        }
+        return true;
+    }
+}
