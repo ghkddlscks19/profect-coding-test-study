cfd84cf3348fe9146ced24a987c569eb5a8f32bc
eunkyung <melon0802@naver.com>
2025-10-31 00:59:27 +0900

    Day 38: 배은경

--- problems/day38-피보나치수/배은경.java
+++ problems/day38-피보나치수/배은경.java
@@ -3,9 +3,8 @@ class Solution {
         long[] fb = new long[n + 1];
         fb[0] = 0;
         fb[1] = 1;
-        fb[2] = 1;
         
-        for(int i = 3; i <= n; i++)
+        for(int i = 2; i <= n; i++)
             fb[i] = (fb[i - 1] + fb[i - 2]) % 1234567; 
         
         int answer = (int)(fb[n] % 1234567);
