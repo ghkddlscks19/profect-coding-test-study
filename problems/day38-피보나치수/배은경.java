cd1f29071ef2e51a55d04cd6192cdcdd23390b45
eunkyung <melon0802@naver.com>
2025-10-31 00:58:31 +0900

    Day 38: 배은경

---
+++ problems/day38-피보나치수/배은경.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int n) {
+        long[] fb = new long[n + 1];
+        fb[0] = 0;
+        fb[1] = 1;
+        fb[2] = 1;
+        
+        for(int i = 3; i <= n; i++)
+            fb[i] = (fb[i - 1] + fb[i - 2]) % 1234567; 
+        
+        int answer = (int)(fb[n] % 1234567);
+        return answer;
+    }
+}
