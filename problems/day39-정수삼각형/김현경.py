dff340d6b4f54a49398ca94a86b0778f03fd61b4
hhkim2021 <khk990619@gmail.com>
2025-10-31 23:12:53 +0900

    Day39: 김현경

---
+++ problems/day39-정수삼각형/김현경.py
@@ -0,0 +1,14 @@
+def solution(triangle):
+    h = len(triangle)
+    for i in range(1, h):
+        for j in range(len(triangle[i])):
+            if j == 0:
+                triangle[i][j] += triangle[i-1][j]
+            elif j == len(triangle[i]) - 1: 
+                triangle[i][j] += triangle[i-1][j-1]
+            else:
+                triangle[i][j] += max(triangle[i-1][j-1], triangle[i-1][j])
+
+    answer = max(triangle[-1])
+    
+    return answer
