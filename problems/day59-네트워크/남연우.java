dd0abbedcc92e8da8217229a7cd1416b4defcd0b
NamYeonW00 <114655545+NamYeonW00@users.noreply.github.com>
2025-11-20 22:38:20 +0900

    Day 59: 남연우

---
+++ problems/day59-네트워크/남연우.java
@@ -0,0 +1,25 @@
+class Solution {
+    static boolean[] visited;
+    
+    public int solution(int n, int[][] computers) {
+        int answer = 0;
+        visited = new boolean[n];
+        
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                answer++;
+                dfs(i, computers);
+            }
+        }
+        return answer;
+    }
+    
+    static void dfs(int n, int[][] computers) {
+        visited[n] = true;
+        for (int i = 0; i < computers.length; i++) {
+            if (computers[n][i] == 1 && !visited[i])
+                dfs(i, computers);
+        }
+        return;
+    }
+}
