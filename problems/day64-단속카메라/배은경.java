a74c27158069c61859482d728253dd1c0c1ae701
eunkyung <melon0802@naver.com>
2025-11-25 14:51:34 +0900

    Day 64: 배은경
    Implement solution to the camera placement problem.

---
+++ problems/day64-단속카메라/배은경.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] routes) {
+        Arrays.sort(routes, (a, b) -> a[1] - b[1]);
+        
+        int answer = 1;
+        int std = routes[0][1];
+        for(int i = 1; i < routes.length; i++)
+            if(routes[i][0] > std) {
+                std = routes[i][1];
+                answer++;
+            }
+        
+        return answer;
+    }
+}
