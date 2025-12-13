62c0498bb999b757293c679f6f18bb58f10a75e8
eunkyung <melon0802@naver.com>
2025-12-13 23:29:59 +0900

    DA

---
+++ problems/day82-2312-수복원하기/배은경.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+        
+        for(int i = 0; i < tc; i++) {
+            int num = Integer.parseInt(br.readLine());
+            int n = 2;
+            int cnt = 0;
+            
+            while(num >= n) {
+                if(num % n == 0) {
+                    cnt++;
+                    num /= n;
+                }else{
+                    if(cnt > 0)
+                        System.out.println(n + " " + cnt);
+                    n++;
+                    cnt = 0;
+                }
+            }
+            
+            if(cnt > 0)
+                System.out.println(n + " " + cnt);
+            if(num > 1){
+                cnt = 1;
+                System.out.println(num + " " + cnt);
+            }
+        }
+    }
+}
