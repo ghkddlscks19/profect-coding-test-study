79df067e3932bf649f4e64b1f4e72c55b5164fe4
eunkyung <melon0802@naver.com>
2025-12-23 14:04:36 +0900

    Day 92: 배은경

---
+++ problems/day92-1535-안녕/배은경.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    static int[][] per;
+    static int n;
+    static int max = 0;
+    public static void DFS(int idx, int status, int happy, boolean[] vst){
+        if(idx > n)
+            return;
+        if(happy > max)
+            max = happy;
+        
+        for(int i = idx; i < n; i++)
+            if(vst[i] == false && status - per[i][0] > 0) {
+                vst[i] = true;
+                DFS(i + 1, status - per[i][0], happy + per[i][1], vst);
+                vst[i] = false;
+            }
+        return;
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        
+        per = new int[n][2];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++)
+            per[i][0] = Integer.parseInt(st.nextToken());
+        
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++)
+            per[i][1] = Integer.parseInt(st.nextToken());
+        
+        Arrays.sort(per, (a, b) -> b[1] - a[1]);
+        boolean[] vst = new boolean[n];
+        DFS(0, 100, 0, vst);
+        System.out.println(max);
+    }
+}
