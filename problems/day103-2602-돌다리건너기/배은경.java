7e61e2917988de51e3bd14578906c230b3a42d21
eunkyung <melon0802@naver.com>
2026-01-04 03:05:45 +0900

    Day 103: 배은경
    ♻️

--- problems/day103-2602-돌다리건너기/배은경.java
+++ problems/day103-2602-돌다리건너기/배은경.java
@@ -8,7 +8,6 @@ class Main {
     static int[][][] vst;
     static int dp(int idx2, int idx1, int ch) {
         int sum = 0;
-        
         if(idx1 == tr.length)
             return 1;
         if(idx2 >= ab.length)
@@ -17,33 +16,25 @@ class Main {
             return vst[idx2][idx1][ch];
         
         for(int i = idx2; i < ab.length; i++)
-            if(ch == 1 && ab[i] == tr[idx1]) {
-                if(vst[idx2][idx1][1] == -1)
-                    sum += dp(i + 1, idx1 + 1, 0);
-            } else if(ch == 0 && db[i] == tr[idx1]) {
-                if(vst[idx2][idx1][0] == -1)
-                    sum += dp(i + 1, idx1 + 1, 1);
-            }
+            if(ch == 1 && ab[i] == tr[idx1])
+                sum += dp(i + 1, idx1 + 1, 0);
+            else if(ch == 0 && db[i] == tr[idx1])
+                sum += dp(i + 1, idx1 + 1, 1);
         
-        if(ch == 0) vst[idx2][idx1][0] = sum;
-        if(ch == 1) vst[idx2][idx1][1] = sum;
+        vst[idx2][idx1][ch] = sum;
         return sum;
     }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String s = br.readLine();
-        String d = br.readLine();
-        String a = br.readLine();
-        
-        tr = s.toCharArray();
-        db = d.toCharArray();
-        ab = a.toCharArray();
+        tr = br.readLine().toCharArray();
+        db = br.readLine().toCharArray();
+        ab = br.readLine().toCharArray();
         vst = new int[db.length][tr.length][2];
+        
         for (int i = 0; i < db.length; i++)
             for (int j = 0; j < tr.length; j++)
                 Arrays.fill(vst[i][j], -1);
         
-        int cnt = dp(0, 0, 0) + dp(0, 0, 1);
-        System.out.print(cnt);
+        System.out.print(dp(0, 0, 0) + dp(0, 0, 1));
     }
 }
